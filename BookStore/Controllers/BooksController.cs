286a9611d76e949ea196d7a2ff834c0f5cd78072
anilcirakoglu <115578970+anilcirakoglu@users.noreply.github.com>
2024-02-09 03:06:52 +0300

    4 commit

--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookStore.Application.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
 
 
 namespace BookStore.Controllers
@@ -8,5 +10,20 @@ namespace BookStore.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        
+
+        readonly private IBookReadRepository _bookReadRepository;
+
+        public BooksController(IBookReadRepository bookReadRepository)
+        {
+            
+            _bookReadRepository = bookReadRepository;
+        }
+        [HttpGet]
+        public void Get() {
+
+            _bookReadRepository.GetAll();
+          }
+
     }
 }
