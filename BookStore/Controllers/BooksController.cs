1fe1887b3c02f1a8f7934dc249d5b4bc309a493a
ANIL CIRAKOGLU <anil.cirakoglu@isik.edu.tr>
2024-02-12 17:00:06 +0300

    14. commit

--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -20,55 +20,25 @@ namespace BookStore.Controllers
         readonly private IBookWriteRepository _bookWriteRepository;
         readonly private IBookReadRepository _bookReadRepository;
         readonly private IPricesReadRepository _priceReadRepository;
+        readonly private IPricesWriteRepository _priceWriteRepository;
+       
          
 
 
-        public BooksController(IBookReadRepository bookReadRepository,IBookWriteRepository bookWriteRepository,IPricesReadRepository pricesReadRepository)
+        public BooksController(IBookReadRepository bookReadRepository,IBookWriteRepository bookWriteRepository,IPricesReadRepository pricesReadRepository,IPricesWriteRepository pricesWriteRepository)
         {
             
             _bookReadRepository = bookReadRepository;
             _bookWriteRepository = bookWriteRepository;
             _priceReadRepository = pricesReadRepository;
+            _priceWriteRepository = pricesWriteRepository;
+            
          
 
 
         }
 
 
-
-        //[HttpGet]
-
-        //public  IActionResult GetAll()
-        //{
-        //    var books = _bookReadRepository.GetAll().ToList();
-        //    var booksWithPrice = new List<object>();
-
-
-
-        //    foreach (var book in books)
-        //    {
-
-        //        var prices = _priceReadRepository.GetAll().Where(p => p.bookid == book.id).Select(p => p.price).ToList();
-        //        var bookWithPrice = new
-        //        {
-        //            book.id,
-        //            book.name,
-        //            book.isbn,
-        //            book.category,
-        //            book.published,
-        //            book.author,
-
-
-        //            prices
-
-
-        //        };
-        //        booksWithPrice.Add(bookWithPrice);
-
-        //    }
-
-        //    return Ok(booksWithPrice);
-        //}
         [HttpGet]
         public IActionResult GetAll() {
             var books = _bookReadRepository.GetAll().ToList();
@@ -100,7 +70,7 @@ namespace BookStore.Controllers
         {
 
             var book= await _bookReadRepository.GetByIdAsync(id);
-            var priceList = _priceReadRepository.GetAll().ToList();
+            var priceList = _priceReadRepository.GetAll().Where(x=>x.bookid==book.id);
             var booksWithPrice = new List<BookModel>();
             var bookWithPrice = new BookModel
             {
@@ -119,13 +89,37 @@ namespace BookStore.Controllers
             return Ok(bookWithPrice);
         }
         [HttpPost("create")]
-        public async Task<ActionResult<Book>> Create(Book todoItem)
+        public async Task<ActionResult<Book>> Create(BookModel book)
         {
-            await _bookWriteRepository.AddAsync(todoItem);
-            await _bookWriteRepository.SaveAsync();
 
-            return CreatedAtRoute( new { id = todoItem.id }, todoItem);
+            var entity = new Book()
+            {
+                id = book.id,
+                name = book.name,
+                isbn = book.isbn,
+                category = book.category,
+                published = book.published,
+                author = book.author
+            };
+
+            var entity2 = new Prices()
+            {
+                id=book.id,
+                bookid = entity.id,
+                price = book.price,
+            };
+
+
+
+            await _bookWriteRepository.AddAsync(entity);
+            await _priceWriteRepository.AddAsync(entity2);
+            
+           await _bookWriteRepository.SaveAsync();
+            await _priceWriteRepository.SaveAsync();
+
+            return CreatedAtRoute( new { id = book.id }, book);
         }
-
+        
+      
     }
 }
--- BookStore/Controllers/UsersController.cs
+++ BookStore/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 ﻿using BookStore.Application.Repositories;
+using BookStore.Domain.Entities;
+using BookStore.Domain.Models;
 using BookStore.Persistence.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +12,37 @@ namespace BookStore.Controllers
     public class UsersController : ControllerBase
     {
         readonly private IUsersReadRepository _usersReadRepository;
-        public UsersController(IUsersReadRepository usersReadRepository)
+        readonly private IUsersWriteRepository _usersWriteRepository;
+        public UsersController(IUsersReadRepository usersReadRepository, IUsersWriteRepository usersWriteRepository)
         {
 
             _usersReadRepository = usersReadRepository;
+            _usersWriteRepository = usersWriteRepository;
         }
         [HttpGet]
         public IActionResult GetAll()
         {
             var book = _usersReadRepository.GetAll();
             return Ok(book);
+        }
+        [HttpPost("create")]
+        public async Task<ActionResult<Users>> createUser(UsersModel usersModel)
+        {
+
+
+            var entity = new Users()
+            {
+
+                id = usersModel.id,
+                name = usersModel.name,
+                surname = usersModel.surname,
+                phonenumber = usersModel.phonenumber,
+                email = usersModel.email
+            };
+            await _usersWriteRepository.AddAsync(entity);
+            await _usersWriteRepository.SaveAsync();
+            return CreatedAtRoute(new { id = usersModel.id }, usersModel);
+
         }
 
     }
